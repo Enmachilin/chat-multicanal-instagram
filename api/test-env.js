b173979a376a23e50ce94a7fb91a8bd33810b6fe
Enmachilin <pentacydesign@gmail.com>
2026-01-18 17:17:49 -0400

    debug: add test-env endpoint

---
+++ api/test-env.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+    res.status(200).json({
+        projectId: process.env.FIREBASE_PROJECT_ID || 'missing',
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL || 'missing',
+        privateKeySet: !!process.env.FIREBASE_PRIVATE_KEY,
+        privateKeyLength: process.env.FIREBASE_PRIVATE_KEY?.length || 0,
+        verifyToken: process.env.VERIFY_TOKEN || 'missing'
+    });
+}
